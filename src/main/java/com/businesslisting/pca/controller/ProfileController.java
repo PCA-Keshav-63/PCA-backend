622494b76c9765b306bc70e87f394fdfe151b139
PCA-Keshav-63 <keshav.pca@gmail.com>
2025-07-02 13:51:57 +0530

    Update ProfileController.java

--- src/main/java/com/businesslisting/pca/controller/ProfileController.java
+++ src/main/java/com/businesslisting/pca/controller/ProfileController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,24 +21,27 @@ public class ProfileController {
     private final ProfileService profileService;
     private final EmailService emailService;
 
-
-    @PostMapping("/register")
+    @PostMapping("/api/v1.0/register")
     @ResponseStatus(HttpStatus.CREATED)
 
-    public ProfileResponse register(@Valid @RequestBody ProfileRequest request){
+    public ProfileResponse register(@Valid @RequestBody ProfileRequest request) {
         System.out.println("Register request received: " + request);
         System.out.println("Register rikloahfkjahs received: " + request.getRole());
-        
+
         System.out.println("Register role : " + request.getRole().getClass().getName());
         ProfileResponse response = profileService.createProfile(request);
-        emailService.sendWelcomeEmail(response.getEmail(),response.getName(),response.getRole());
+        emailService.sendWelcomeEmail(response.getEmail(), response.getName(), response.getRole());
         return response;
     }
 
-
-    @GetMapping("/profile")
-    public ProfileResponse getProfile(@CurrentSecurityContext(expression = "authentication?.name")String email){
+    @GetMapping("/api/v1.0/profile")
+    public ProfileResponse getProfile(@CurrentSecurityContext(expression = "authentication?.name") String email) {
         profileService.getProfile(email);
-    return profileService.getProfile(email) ;
+        return profileService.getProfile(email);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> root() {
+        return ResponseEntity.ok("API is running");
     }
 }
